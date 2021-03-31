01dc49ba139294aa35e60a4ee2e362060d0d745a
Vanilj <robindavidsson@outlook.com>
2021-03-31 09:18:38 +0200

    added comments

--- solution_2wordladders.java
+++ solution_2wordladders.java
@@ -16,37 +16,51 @@ class solver {
 
         int n = scanner.nextInt();
         int q = scanner.nextInt();
+
+        // Innehåller alla start och slut noder i par
         NodePair[] sp = new NodePair[q];
 
+        // Grafen
         Graph G = new Graph();
 
+        // Lägger till alla orden som noder (inte kopplade än)
         for (int i = 0; i < n; i++) {
             String num = scanner.next();
             G.nodes.add(new node(num.toCharArray()));
         }
 
+        // Hittar var nod i Graph och lägger till dem som par
         for (int i = 0; i < q; i++) {
             node n1 = G.getNodeByString(scanner.next());
             node n2 = G.getNodeByString(scanner.next());
             sp[i] = new NodePair(n1, n2);
         }
 
+        // Kollar om någon annan nod innehåller node's fyra sista bokstäver
         for (int i = 0; i < G.nodes.size(); i++) {
             node node = G.nodes.get(i);
 
+            // Går igenom alla andra nodes, i != j
             for (int j = 0; j < G.nodes.size(); j++) {
                 if (i != j) {
+                    // Den andra noden, ordet som noden innehåller och antalet korrekta bokstäver
+                    // som den andra noden innehåller
                     node other = G.nodes.get(j);
-                    String sword = other.fullWord();
+                    String oWord = other.fullWord();
                     int correct = 0;
 
+                    // Kollar för var bokstav i node (char c) om den finns i den andra nodens ord
                     for (int k = 1; k < node.text.length; k++) {
                         char c = node.text[k];
+                        // Kollar om antalet gånger som c förekommer i noderna
+                        // Den första är 1 till 4 medans den andra är 0 till 4
                         if (node.charCount(c, 1) > other.charCount(c, 0)) {
                             correct = 0;
                             break;
                         } else {
-                            if (sword.indexOf(c) != -1) {
+                            // Om den andra nodens ord innehåller c, dvs index != -1, räknar vi den. Annars
+                            // avbryter vi direkt.
+                            if (oWord.indexOf(c) != -1) {
                                 correct++;
                             } else {
                                 correct = 0;
@@ -54,55 +68,75 @@ class solver {
                             }
                         }
                     }
+                    // Om other nodtexten innehöll lika många correkta som nodens text har i längd -
+                    // 1 så lägger vi till den som en granne.
                     if (correct == node.text.length - 1) {
                         node.nbs.add(other);
                     }
-                    correct = 0;
+                    // correct = 0;
                 }
             }
         }
 
+        // För var par gör vi bredden först sökning för att hitta kortast sträcka från
+        // den ena noden till den andra.
         for (NodePair nodePair : sp) {
             BFS(G, nodePair.n1, nodePair.n2);
         }
     }
 
     private void BFS(Graph G, node s, node t) {
-        if (s.fullWord().equals(t.fullWord())){
+        // ordet vi vill nå
+        String endWord = t.fullWord();
+
+        // Om vi är på den rätta noden behöver vi inte gå någonstans och skriver ut 0
+        // if (s.fullWord().equals(t.fullWord())) {
+        if (s.Compare(endWord)) {
             System.out.println("0");
             return;
         }
-        for (node n : G.nodes){
+
+        // Resetar alla värden
+        for (node n : G.nodes) {
             n.pred = null;
             n.depth = 0;
         }
-
-        String tWord = t.fullWord();
-
+        
         ArrayList<node> q = new ArrayList<node>();
+        // Lägger till startnoden
         q.add(s);
 
         while (q.size() > 0) {
             node nodeMain = q.remove(0);
-            for (node otherNode : nodeMain.nbs) {
-                if (otherNode.pred == null) {
-                    if (tWord.equals(otherNode.fullWord())) {
+            for (node grannNode : nodeMain.nbs) {
+                // Om pred == null så har vi inte varit vid den noden innan
+                if (grannNode.pred == null) {
+                    // Kollar om g ord är samma som slutordet
+                    if (endWord.equals(grannNode.fullWord())) {
+                        // if (t.Compare(grannNode.fullWord())){
                         System.out.println(nodeMain.depth + 1);
                         return;
                     }
-                    otherNode.depth = nodeMain.depth + 1;
-                    q.add(otherNode);
-                    otherNode.pred = nodeMain;
+                    // Om det är fel nod går vi vidare
+                    // Sätter den andra nodens djup som vår mains djup + 1
+                    grannNode.depth = nodeMain.depth + 1;
+                    // lägger till i kön
+                    q.add(grannNode);
+                    // sätter företrädarnoden till nodeMain i den grannoden
+                    grannNode.pred = nodeMain;
                 }
             }
         }
+        // Hittar vi ingen väg så skriver vi ut
         System.out.println("Impossible");
     }
 }
 
 class Graph {
+    // Lista med alla noder
     ArrayList<node> nodes = new ArrayList<>();
 
+    // Hittar en nod från en string
     public node getNodeByString(String string) {
         for (node node : nodes) {
             if (node.Compare(string)) {
@@ -114,6 +148,7 @@ class Graph {
 }
 
 class node {
+    // Nodens egenskaper
     public char[] text;
     public ArrayList<node> nbs = new ArrayList<>();
     public node pred;
@@ -123,10 +158,8 @@ class node {
         text = t;
     }
 
-    public String lastWord() {
-        return fullWord().substring(1);
-    }
-
+    // räknar antalet gånger en karaktär förekommer i nodens string
+    // Tar också in int f som är en förskjutning på forloopen
     public int charCount(char c, int f) {
         int count = 0;
         for (int i = f; i < text.length; i++)
@@ -135,10 +168,17 @@ class node {
         return count;
     }
 
+    // Returnerar hela ordet förutom den första bokstaven, dvs de fyra sista
+    public String lastWord() {
+        return fullWord().substring(1);
+    }
+
+    // Returnerar hela ordet som en sträng
     public String fullWord() {
         return new String(text);
     }
 
+    // Kollar om någon string s är samma som denna noden
     public boolean Compare(String s) {
         if (fullWord().equals(s))
             return true;
@@ -147,11 +187,12 @@ class node {
 
 }
 
-class NodePair{
+class NodePair {
+    // Nodpar
     public node n1;
     public node n2;
 
-    public NodePair(node n1, node n2){
+    public NodePair(node n1, node n2) {
         this.n1 = n1;
         this.n2 = n2;
     }
