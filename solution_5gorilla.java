56c78d963288a73a0f6d8f6df47b74c50aed3b11
Vanillj <robindavidsson@outlook.com>
2021-05-11 18:47:40 +0200

    done?

--- solution_5gorilla.java
+++ solution_5gorilla.java
@@ -1,7 +1,5 @@
 import java.io.BufferedInputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -10,14 +8,13 @@ import java.util.Scanner;
 public class solution_5gorilla {
 
     static Integer[][] connection;
-    static Integer[][] optvals;
+    static Integer[][] opt;
     static HashMap<Character, Letter> lettersC = new HashMap<>();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(new BufferedInputStream(System.in));
 
         LinkedList<Query> queries = new LinkedList<>();
-        //HashMap<Integer, Letter> letters = new HashMap<>();
         int count = 0;
 
         while (scanner.hasNext()) {
@@ -27,7 +24,7 @@ public class solution_5gorilla {
             String s = scanner.next();
 
             Letter l = new Letter(s.charAt(0));
-            //letters.put(count, l);
+            l.v = count;
             lettersC.put(s.charAt(0), l);
             count++;
         }
@@ -71,33 +68,74 @@ public class solution_5gorilla {
         Iterator<Query> it = queries.iterator();
         while (it.hasNext()) {
             Query query = it.next();
-            optvals = new Integer[query.query1.size()+1][query.query2.size()+1];
+            int q1Length = query.query1.size();
+            int q2Length = query.query2.size();
 
-            opt(query.query1.size(), query.query2.size(), query);
-            //optAlt(query);
+            opt = new Integer[q1Length+1][q2Length+1];
 
-            System.out.println(Arrays.deepToString(optvals).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
+            opt(q1Length, q2Length, query);
+
+            //System.out.println(Arrays.deepToString(optvals).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
+
+            int longest = Math.max(query.query2.size(), query.query1.size());
+
+            int i = query.query1.size();
+            int j = query.query2.size();
+            StringBuilder sb = new StringBuilder(longest);
+            StringBuilder sb2 = new StringBuilder(longest);
+
+            while (i > 0 && j > 0) {
+                Character ci = query.query1.get(i-1);
+                Letter li = lettersC.get(ci);
+
+                Character cj = query.query2.get(j-1);
+                Letter lj = lettersC.get(cj);
+                
+                if (i > 0 && j > 0 && opt[i][j] == opt[i-1][j-1] + connection[li.v][lj.v]) {
+                    sb.append(query.query1.get(--i));
+                    sb2.append(query.query2.get(--j));
+                }
+                else if (i > 0 && opt[i][j] + 4  == opt[i - 1][j]) {
+                    sb.append(query.query1.get(j-1));
+                    sb2.append('*');
+                    i--;
+                }
+                else if (j > 0 && opt[i][j] + 4 == opt[i][j-1]) {
+                    sb2.append(query.query2.get(j-1));
+                    sb.append('*');
+                    j--;
+                }
+            }
+
+            while (i > 0) {
+                sb.append(query.query1.get(i));
+                sb2.append('*');
+                i--;                
+            }
+            while (j > 0) {
+                sb2.append(query.query2.get(j));
+                sb.append('*');
+                i--;                
+            }
+
+            String st = sb.reverse().toString();
+            String st2 = sb2.reverse().toString();
             
-            StringBuilder stringBuilder = new StringBuilder();
-
-            System.out.println(c + " : " + (query.query1.size() * query.query2.size()));
+            System.out.println(st + " " + st2);
         }
     }
 
-    static int c = 0;
-
     static int opt(int i, int j, Query query) {
         int delta = -4;
-        c++;
         if (i == 0 && j == 0) {
-            optvals[i][j] = 0;
+            opt[i][j] = 0;
             return 0;
         }
         if (i == 0) {
-            optvals[0][j] = j * delta;
+            opt[0][j] = j * delta;
             return j * delta;
         } else if (j == 0) {
-            optvals[i][0] = i * delta;
+            opt[i][0] = i * delta;
             return i * delta;
         }
 
@@ -112,34 +150,8 @@ public class solution_5gorilla {
         int dopt1 = delta + opt(i, j - 1, query);
         int dopt2 = delta + opt(i - 1, j, query);
 
-        optvals[i][j] = Math.min(Math.min(dopt1, dopt2), connopt);
-        
-        return optvals[i][j];
-    }
-
-    static void optAlt(Query q) {
-        int delta = -4;
-        for (int i = 0; i <= q.query1.size(); i++) {
-            optvals[i][0] = i * delta;
-        }
-        for (int i = 0; i <= q.query2.size(); i++) {
-            optvals[0][i] = i * delta;
-        }
-        for (int i = 1; i <= q.query1.size(); i++) {
-            for (int j = 1; j <= q.query2.size(); j++) {
-                int dopt2 = delta + optvals[i - 1][j];
-                int dopt1 = delta + optvals[i][j - 1];
-
-                Character ci = q.query1.get(i-1);
-                Letter li = lettersC.get(ci);
-
-                Character cj = q.query2.get(j-1);
-                Letter lj = lettersC.get(cj);
-
-                int connopt = connection[li.v][lj.v] + optvals[i - 1][j - 1];
-                optvals[i][j] = Math.min(Math.min(dopt1, dopt2), connopt);
-            }
-        }
+        opt[i][j] = Math.max(Math.max(dopt1, dopt2), connopt);
+        return opt[i][j];
     }
 
 }
@@ -162,3 +174,28 @@ class Letter {
         this.c = c;
     }
 }
+
+/*static void optAlt(Query q) {
+        int delta = -4;
+        for (int i = 0; i <= q.query1.size(); i++) {
+            opt[i][0] = i * delta;
+        }
+        for (int i = 0; i <= q.query2.size(); i++) {
+            opt[0][i] = i * delta;
+        }
+        for (int i = 1; i <= q.query1.size(); i++) {
+            for (int j = 1; j <= q.query2.size(); j++) {
+                int dopt2 = delta + opt[i - 1][j];
+                int dopt1 = delta + opt[i][j - 1];
+
+                Character ci = q.query1.get(i-1);
+                Letter li = lettersC.get(ci);
+
+                Character cj = q.query2.get(j-1);
+                Letter lj = lettersC.get(cj);
+
+                int connopt = connection[li.v][lj.v] + opt[i - 1][j - 1];
+                opt[i][j] = Math.min(Math.min(dopt1, dopt2), connopt);
+            }
+        }
+    }*/
