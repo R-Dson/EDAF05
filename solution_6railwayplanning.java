394011f10d5257baec144e6cdef4b09a588da975
Vanillj <robindavidsson@outlook.com>
2021-05-26 14:13:17 +0200

    Done

--- solution_6railwayplanning.java
+++ solution_6railwayplanning.java
@@ -1,297 +1,192 @@
-import java.io.BufferedInputStream;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-class solution_6railwayplanning {
+public class sol_temp {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(new BufferedInputStream(System.in));
+        Scanner scanner = new Scanner(System.in);
         int N, M, C, P;
+
         N = scanner.nextInt(); // antalet noder
         M = scanner.nextInt(); // antalet kanter
         C = scanner.nextInt(); // antalet personer
         P = scanner.nextInt(); // antalet vägar
 
-        // HashMap<Integer, Node> nodes = new HashMap<>(N);
-        Graph graph = new Graph(M, N);
+        Node[] g = new Node[N];
+
+        // deklarerar variablerna node
+        for (int i = 0; i < N; i++) {
+            g[i] = new Node();
+            g[i].ind = i;
+        }
+
+        int[][] paths = new int[M][3];
 
         for (int i = 0; i < M; i++) {
+
             int u = scanner.nextInt();
             int v = scanner.nextInt();
             int c = scanner.nextInt();
 
-            Node nodeu = graph.nodes.get(u);
-            Node nodev = graph.nodes.get(v);
+            // bygger upp vägarna mellan noderna
+            paths[i][0] = u; // nod 1
+            paths[i][1] = v; // nod 2
+            paths[i][2] = c; // kapacitansen
+            Edge e = new Edge(c, 0);
 
-            if (nodeu == null) {
-                nodeu = new Node(u);
-                graph.nodes.put(u, nodeu);
-            }
-            if (nodev == null) {
-                nodev = new Node(v);
-                graph.nodes.put(v, nodev);
-            }
+            g[u].neighbours.put(g[v], e);
+            g[v].neighbours.put(g[u], e);
 
-            Edge e1 = new Edge(nodeu, nodev, c, i);
-            Edge e2 = new Edge(nodev, nodeu, c, i);
-
-            nodeu.edges.put(v, e1);
-            nodev.edges.put(u, e2);
-
-            graph.AddToList(e1);
-            graph.AddToList(e2);
         }
 
-        Integer[] remove = new Integer[P];
+        // tar bort alla vägarna som ska tas bort
+        int[] removed = new int[P];
         for (int i = 0; i < P; i++) {
-            remove[i] = scanner.nextInt();
+            removed[i] = scanner.nextInt();
+
+            // hittar vägen
+            int[] TempPath = paths[removed[i]];
+            // hittar noderna till vägen
+            int nodeNum1 = TempPath[0];
+            int nodeNum2 = TempPath[1];
+
+            // tar bort vägen i var nods neighbours lista
+            // (neighbours är hashmap med värdena <Node, Edge>)
+            g[nodeNum1].neighbours.remove(g[nodeNum2]);
+            g[nodeNum2].neighbours.remove(g[nodeNum1]);
+
         }
-        int lowest = Integer.MAX_VALUE;
-        int count = 0;
-        //graph.CreateRGraph();
+        scanner.close();
 
-        //tar bort kanter
-        //for (int i = 1; i < 16; i++) {
-            //graph.removeEdgei(remove[remove.length - i]);
-        //}
+        // totala flödet
+        int totFlow = 0;
+        // vi lägger nu tillbaka en åt gången tills
+        // vi når den lägsta gränsen för C
+        for (int j = removed.length - 1; j >= 0; j--) {
 
-        //int tempis1 = ford_fulkerson(graph, graph.nodes.get(0), graph.nodes.get(graph.nodes.size() - 1), C);
-        //graph.CreateRGraph();
-        //graph.removeEdgei(remove[remove.length - 1]);
-        //int tempis = ford_fulkerson(graph, graph.nodes.get(0), graph.nodes.get(graph.nodes.size() - 1), C);
+            // hittar vägen, nodernas värden och kapaciteten
+            int[] TempPath = paths[removed[j]];
+            int nodeNum1 = TempPath[0];
+            int nodeNum2 = TempPath[1];
+            int c = TempPath[2];
 
-        for (int i = 0; i < remove.length; i++) {
-            graph.CreateRGraph();
-            graph.removeEdgei(remove[remove.length - i - 1]);
-            int tempi = ford_fulkerson(graph, graph.nodes.get(0), graph.nodes.get(graph.nodes.size() - 1), C);
+            // sätter in två nya vägar som vi tog bort tidigare
+            g[nodeNum1].neighbours.put(g[nodeNum2], new Edge(c, 0));
+            g[nodeNum2].neighbours.put(g[nodeNum1], new Edge(c, 0));
 
-            if (tempi > -1) {
-                lowest = tempi;
-                count = i;
-            } else {
-                break;
+            // kollar om vi kan få ett flöde >= C med ford Fulkerson
+            int maxflow = Integer.MAX_VALUE;
+            while (maxflow != 0) {
+                maxflow = fordFulkerson(g, 0, N - 1);
+                totFlow += maxflow;
             }
 
+            if (totFlow >= C) {
+                System.out.println(j + " " + totFlow);
+                return;
+            }
         }
-
-        System.out.println(count + " " + lowest);
     }
 
-    public static int ford_fulkerson(Graph G, Node s, Node t, int c) {
-        G.SetToZero();
-        // G.CreateRGraph();
-        int maxflow = 0;
-        while (BFS(G.rgraph, s, t)) {
-            int minc = Integer.MAX_VALUE;
-            Node temp = t;
-            while (temp.pred != null) {
-                int tempc = temp.pred.GetEdge(temp.value).cap;
-                if (tempc < minc)
-                    minc = tempc;
-                temp = temp.pred;
+    public static int fordFulkerson(Node[] g, int s, int t) {
+
+        int total = 0;
+        // Om vi hittar en väg från s till t får vi true från BFS
+        while (BFS(g, s, t)) {
+
+            // delta blir den lägsta kapacitet längst vägen
+            int delta = Integer.MAX_VALUE;
+            Node NbrNode = g[t];
+
+            // går igenom vägen från t till s för att hitta lägsta kapaciteten
+            while (NbrNode.ind != s) {
+                // hämtar kanten mellan noderna
+                Edge edge = NbrNode.pred.neighbours.get(NbrNode);
+                // tillgängligt flöde
+                int diff = edge.c - edge.f;
+                //
+                delta = Math.min(diff, delta);
+                NbrNode = NbrNode.pred;
             }
+            //adderar den lägsta kapacitansen till totala vägens flöde
+            total += delta;
 
-            temp = t;
-            while (temp.pred != null) {
-                Edge edge = temp.pred.GetEdge(temp.value);
-
-                Node d = edge.d, srs = edge.s;
-                d.GetEdge(srs.value).cap += minc;
-                srs.GetEdge(d.value).cap -= minc;
-
-                temp = temp.pred;
+            //ökar flödet längs kanterna
+            NbrNode = g[t];
+            while (NbrNode.ind != s) {
+                Edge e1 = NbrNode.pred.neighbours.get(NbrNode);
+                e1.f = e1.f + delta;
+                NbrNode = NbrNode.pred;
             }
-            maxflow += minc;
-            // kan använda t.pred till att hitta vägen tillbaka
-
         }
-        return maxflow;
+        
+        return total;
     }
 
-    private static boolean BFS(RGraph G, Node s, Node t) {
-        // talet vi vill nå
-        int i = t.value;
+    public static boolean BFS(Node[] g, int s, int t) {
 
-        // Om vi är på den rätta noden behöver vi inte gå någonstans
-        if (s.value == t.value) {
+        // om vi är på rätt nod gör vi inget
+        if (t == s) {
             return true;
         }
 
-        // Resetar alla värden
-        Iterator<Node> itn = G.graph.nodes.values().iterator();
-        while (itn.hasNext()) {
-            Node n = itn.next();
-            n.pred = null;
+        // sätter alla noder till obesökta
+        for (Node node : g) {
+            node.visited = false;
         }
 
-        LinkedList<Node> q = new LinkedList<Node>();
-        // Lägger till startnoden
-        q.add(s);
+        g[s].visited = true;
 
-        while (q.size() > 0) {
-            Node nodeMain = q.poll();
-            nodeMain.visited = true;
-            Iterator<Integer> it = nodeMain.edges.keySet().iterator();
+        LinkedList<Node> q = new LinkedList<>();
+        // Lägger till start noden i q
+        q.add(g[s]);
 
-            while (it.hasNext()) {
-                Integer grannI = it.next();
-                Node grannNode = G.graph.nodes.get(grannI);
-                Edge edge = nodeMain.GetEdge(grannI);
-                // Om pred == null så har vi inte varit vid den noden innan
-                if (grannNode.pred == null && edge.cap > 0 && !grannNode.visited) {
-                    // Kollar om g ord är samma som slutordet
-                    if (i == grannNode.value) {
-                        grannNode.pred = nodeMain;
+        while (!q.isEmpty()) {
+            // hämtar första elementet i q
+            Node v = q.poll();
+
+            // går igenom alla grannoder
+            for (Node NbrNode : v.neighbours.keySet()) {
+                Edge e = v.neighbours.get(NbrNode);
+
+                // begär att vi inte besökt noden innan och att kanten
+                // mellan har en kapacitet större än flödet
+                if (!NbrNode.visited && e.c > e.f) {
+                    NbrNode.visited = true;
+
+                    // pred ger oss vägen tillbaks till start noden
+                    NbrNode.pred = v;
+
+                    if (NbrNode.ind == t) {
                         return true;
                     }
-                    // Om det är fel nod går vi vidare
-                    // lägger till i kön
-                    q.addLast(grannNode);
-                    // sätter företrädarnoden till nodeMain i den grannoden
-                    grannNode.pred = nodeMain;
+
+                    // har vi fel nod lägger vi till den i q och
+                    // går igenom dens grannar
+                    q.add(NbrNode);
                 }
             }
         }
+
+        // väg saknas
         return false;
-    }
-
-}
-
-class Graph {
-    private int M;
-    private ArrayList<Edge> edges;
-    public HashMap<Integer, Node> nodes;
-
-    public RGraph rgraph;
-
-    public Graph(int M, int N) {
-        edges = new ArrayList<>(M);
-        nodes = new HashMap<>(N);
-        this.M = M;
-    }
-
-    public void CreateRGraph() {
-        rgraph = new RGraph(M, this);
-        rgraph.AddAll(edges);
-
-        /*for (Edge edge : listEdgesDest) {
-            edge.d.AddEdge(edge.source, edge);
-        }
-        for (Edge edge : listEdgesSource) {
-            edge.s.AddEdge(edge.dest, edge);
-        }*/
-        //listEdgesDest.clear();
-        //listEdgesSource.clear();
-    }
-
-    public void AddToList(Edge e) {
-        edges.add(e);
-    }
-
-    public void AddToBoth(Edge e) {
-        AddToList(e);
-        rgraph.AddToList(e);
-    }
-
-    public void SetToZero() {
-        edges.forEach(e -> e.flow = 0);
-
-    }
-
-    ArrayList<Edge> listEdgesDest = new ArrayList<>();
-    ArrayList<Edge> listEdgesSource = new ArrayList<>();
-
-    public void removeEdgei(int i) {
-        Iterator<Edge> it = edges.iterator();
-        while (it.hasNext()) {
-            Edge e = it.next();
-            if (e.index == i) {
-                Node d = e.d;
-                Node s = e.s;
-
-                listEdgesDest.add(d.edges.remove(s.value));
-                listEdgesSource.add(s.edges.remove(d.value));
-            }
-        }
-    }
-
-}
-
-class RGraph {
-
-    private ArrayList<Edge> RList;
-
-    public Graph graph;
-
-    public RGraph(int M, Graph G) {
-        RList = new ArrayList<>(M);
-        graph = G;
-    }
-
-    public void AddToList(Edge e) {
-        RList.add(e);
-    }
-
-    public void AddAll(Collection<Edge> c) {
-        RList.addAll(c);
-    }
-
-}
-
-class Edge {
-    int source;
-    int dest;
-    int cap;
-    int index;
-    int flow;
-
-    Node s;
-    Node d;
-
-    public boolean IsBackwards = false;
-
-    public Edge(Node s, Node d, int c, int index) {
-        this.s = s;
-        source = s.value;
-        this.d = d;
-        dest = d.value;
-        cap = c;
-        this.index = index;
-        flow = 0;
     }
 }
 
 class Node {
-    int value;
+    HashMap<Node, Edge> neighbours = new HashMap<Node, Edge>();
     Node pred;
+    boolean visited;
+    int ind;
+}
 
-    boolean visited = false;
+class Edge {
+    int c;
+    int f;
 
-    HashMap<Integer, Edge> edges = new HashMap<>();
-
-    public Node(int value) {
-        this.value = value;
-    }
-
-    public void AddEdge(int i, Edge e) {
-        edges.put(i, e);
-    }
-
-    public Edge GetEdge(int i) {
-        return edges.get(i);
-    }
-
-    public Node GetNodeFromEdge(int i) {
-        if (i == value)
-            return this;
-
-        Edge e = edges.get(i);
-        if (e.source == i)
-            return e.s;
-        else
-            return e.d;
+    public Edge(int c, int f) {
+        this.c = c;
+        this.f = f;
     }
 }
